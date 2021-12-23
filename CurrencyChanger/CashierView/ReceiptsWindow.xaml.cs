92ed2027b6d99bef80af740a22feefa7cf51fe58
Artyom Yankin <artyom.yankin@omegasoftware.eu>
2021-12-23 16:03:50 +0300

    Added cashier views with updating currencies, recent history

---
+++ CurrencyChanger/CashierView/ReceiptsWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using CurrencyChanger.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CurrencyChanger
+{
+    /// <summary>
+    /// Interaction logic for ReceiptsWindow.xaml
+    /// </summary>
+    public partial class ReceiptsWindow : Window
+    {
+        private readonly string filesPath = "C:/Users/Artyom Yankin/Desktop/WPF/CurrencyChanger/Receipts/";
+        public ReceiptsWindow()
+        {
+            InitializeComponent();
+        }
+        public void LoadPhisicalFiles(object c, RoutedEventArgs a)
+        {
+            var files = new List<Files>();
+            var phisicalFiles = Directory.GetFiles(filesPath);
+            foreach (var fl in phisicalFiles)
+            {
+                var finfo = new System.IO.FileInfo(fl);
+                int fileSize = (int)finfo.Length;
+                string fileName = finfo.Name;
+                Files f = new Files();
+                f.Name = fileName;
+                bListFiles.Items.Add(f);
+                bListFiles.Items.Add(new CheckBox()
+                {
+                    Content = f.Name
+                });
+                files.Add(f);
+            }
+        }
+    }
+}
